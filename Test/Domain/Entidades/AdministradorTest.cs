d26a97727b25475f564598acfaa60d694c835010
Silvia Frigatto <silvia.frigatto@gmail.com>
2024-11-08 23:08:09 -0300

    Replicando o projeto

--- Test/Domain/Entidades/AdministradorTest.cs
+++ Test/Domain/Entidades/AdministradorTest.cs
@@ -14,13 +14,13 @@ public class AdministradorTest
         // Act
         adm.Id = 1;
         adm.Email = "teste@teste.com";
-        adm.Senha = "teste";
+        adm.Senha = "teste123";
         adm.Perfil = "Adm";
 
         // Assert
         Assert.AreEqual(1, adm.Id);
         Assert.AreEqual("teste@teste.com", adm.Email);
-        Assert.AreEqual("teste", adm.Senha);
+        Assert.AreEqual("teste123", adm.Senha);
         Assert.AreEqual("Adm", adm.Perfil);
     }
 }
